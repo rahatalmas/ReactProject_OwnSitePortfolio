72ed48bfbb4293241da0d7a1bc876ae3db52c921
rahat_almas <rahat.almas5599@gmail.com>
2021-06-17 23:05:23 +0600

    3rdcommit

--- src/components/BlogsParts/Blogs.js
+++ src/components/BlogsParts/Blogs.js
@@ -1,24 +1,20 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Blogs.css'
 const Blogs = ()=>{
     
-    const [blogs,setblogs] = useState([
-        {
-            title:"parrots",
-            snippet:"food routine of Pattots",
-            body:"sometext........"
-        },
-        {
-            title:"life",
-            snippet:"meaning of life ?",
-            body:"life is an adventurus journey"
-        },
-        {
-            title:"something..",
-            snippet:"some text...",
-            body:"some text..."
-        }
-    ]);
+    const [datas,setdata] = useState('');
+
+    useEffect(()=>{
+        fetch("http://localhost:8000/Blogs").then(
+            (res)=>{
+              return res.json();
+            }
+        ).then(
+          (data)=>{
+              setdata(data);
+          }
+        );
+    },[])
 
     return(
         <div className="blog-container">
@@ -27,12 +23,12 @@ const Blogs = ()=>{
                     Menu
                 </div>
                 <div className="b-m">
-                     {
-                         blogs.map((blog)=>(
+                {
+                       datas &&  datas.map((data)=>(
                              <div className="blog">
-                                 <h3>{blog.title}</h3>
-                                 <h4>{blog.snippet}</h4>
-                                 <p>{blog.body}</p>
+                                 <h3>{data.title}</h3>
+                                 <h4>{data.snippet}</h4>
+                                 <p>{data.body}</p>
                              </div>
                          ))
                      }
